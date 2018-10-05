1d6079c56e43643b96b51f574eed8df5de0f3377
OliverKostic <oliver.kostic11@gmail.com>
2018-10-05 15:54:34 +0200

    db

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const app = express();
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const router = express.Router();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://Oliver:hranolkz1@ds223253.mlab.com:23253/issues?authMechanism=SCRAM-SHA-1');
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log('MongoDB database connection established successfully!');
+});
+
 
 const issue = {
     title: "Hello"
