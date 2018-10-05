e42ae1a703895bcba5a39f318a19f1dbd27e6494
OliverKostic <oliver.kostic11@gmail.com>
2018-10-05 10:44:29 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+const issue = {
+    title: "Hello"
+};
+
+
+app.get('/', (req, res) => {
+    console.log('issue recieved');
+    res.json(issue);
+});
+
+
+
+
+app.listen(process.env.PORT || 8080, () => console.log('Express server running on port 8080'));
