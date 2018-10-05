49df70df95bf0c1cb7a4893c6f6c9e54f8606680
OliverKostic <oliver.kostic11@gmail.com>
2018-10-05 16:03:17 +0200

    imports

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
+const bodyParser = require('body-parser');
 const express = require('express');
-const app = express();
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
+const app = express();
 const router = express.Router();
 
-app.use(cors());
+
 app.use(bodyParser.json());
 
 mongoose.connect('mongodb://Oliver:hranolkz1@ds223253.mlab.com:23253/issues?authMechanism=SCRAM-SHA-1');
